376c746483c1852c437fed5d57aa416c23c1b18b
Craig Wang <wc641354337@gmail.com>
2018-08-26 14:58:08 +0200

    beta2 shiny

--- shiny/server.R
+++ shiny/server.R
@@ -8,6 +8,7 @@ library(tmaptools)
 library(raster)
 library(osmdata)
 library(sf)
+library(fmsb)
 # server ------------------------------------------------------------------
 load("./data/Criteria_per_Cell.RData")
 source("helpers.R")
@@ -18,9 +19,9 @@ function(input, output, session) {
   output$out_var_os <- renderUI({
     if (input$add_varos){
       selectInput("var_os", label = "Points of Interest",
-                  choices = c("bar","kindergarten"), selected = "bars")} else {}
+                  choices = c("bar","kindergarten","pub","nightclub","cafe","restaurant","school","college",
+                              "bus_station","parking","atm"), selected = "bars")} else {}
   })
-  
     output$visplot <- renderLeaflet({
     user <- c(table(base::cut(input$var_age, breaks = c(-Inf, 6, 15, 19, 24, 44, 64, 79, Inf))), # age
               if(input$var_gen == "Male"){c(1,0)}else if(input$var_gen == "Female"){c(0,1)}else{c(0,0)}, # gender
@@ -29,7 +30,7 @@ function(input, output, session) {
                        "gewerbe_per","gesundheit_per","bau_per","sonstdl_per","inform_per",    
                        "unterricht_per","verkehr_per","uebrige_per"), function(x) x == input$var_work)) # work
     
-    dat$score <- calc_score(dat[,3:27], as.vector(user))
+    dat$score <- percentile(calc_score(dat[,3:27], as.vector(user)))
     coordinates(dat) <- ~ ha_x + ha_y
     # coerce to SpatialPixelsDataFrame
     gridded(dat) <- TRUE
@@ -45,11 +46,11 @@ function(input, output, session) {
     pop_layers2 <- projectRaster(pop_layers, crs="+init=epsg:4326 +proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +units=m +no_defs")
     
     tm <- tm_shape(pop_layers2) +
-      tm_raster("score", palette = "Spectral", title = "Inhabitants per 100m2", n = 10) 
+      tm_raster("score", palette = "Oranges", title = "Score", style = "fixed", breaks = seq(0,100,by = 10), alpha = 0.6) 
     if (input$add_varos){
       req(input$var_os)
       tm <- tm + qtm(st_geometry(osmdata_sf(add_osm_feature(opq = opq(bbox = c(8.35768, 47.15944,8.984941,47.694472)), key = 'amenity', value = input$var_os))$osm_points),
-                     symbols.size = 0.05)
+                     symbols.size = 0.025)
     } 
       tmap_leaflet(tm) 
       
--- shiny/ui.R
+++ shiny/ui.R
@@ -27,7 +27,8 @@ dashboardPage(
   dashboardBody(
     tabItems(
       tabItem(tabName = "vis",
-              h2("Input your preference/profile"),
+              h4("Where should you live in canton Zurich?"),
+              h4("Do you want to live in an area where the inhabitants are similar (or maybe dissimilar) to you? With this app, you can find these areas!"),
               fluidRow(
                 column(width = 4,
                        numericInput("var_age", "Age", min = 1, max = 100, value=30, step = 1),
@@ -38,6 +39,10 @@ dashboardPage(
                                    choices = c(colnames(dat)[15:25], " "), selected = " "),
                        checkboxInput("add_varos","Add points of interest", value = FALSE),
                        uiOutput("out_var_os"),
+                       p("How to use the app:
+Construct your profile by setting a value for one or several criteria of interest. We then compute a score and display it on a map of the canton of Zurich. A higher score indicates the areas in canton Zurich that most correspond to your profile."),
+                       p("For instance, if you pick the factors 'Swiss' and 'Age 0-6', we will highlight on a map of canton Zurich the areas where the percentage of the inhabitants that are Swiss and are aged between 0 and 6 is higher than in the rest of the canton. Those are the areas where you might want to live!"),
+                       p(" "),
                        actionButton("contact"," Contact Us", icon = icon("address-card"), 
                                     class = "btn-primary",style = "color: white;",
                                     onclick ="window.open('https://hack.twist2018.ch/project/18', '_blank')")
