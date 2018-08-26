2e2e73da4c0c81cb9fd8f1973a289f885f08858d
Craig Wang <wc641354337@gmail.com>
2018-08-26 14:02:57 +0200

    beta app

--- shiny/helpers.R
+++ shiny/helpers.R
@@ -1,26 +1,7 @@
-plotPop <- function(data, qname, year, month){
-  # subset the data
-  dat <- subset(data, QuarLang == qname & Year == year & StichtagDatMM == month)
-  # plot the population
-  ggplot(dat, aes(x = Gender, y = AnzBestWir, alpha = Origin, fill = Gender)) + 
-    geom_bar(stat = "identity", position = "stack", color="black", width = 0.9) +
-    facet_wrap(~AlterV20Kurz, nrow = 1, strip.position = "bottom") +
-    labs(x = "Age Group", y = "Population", title = paste0("Total Population in ",qname,": ",sum(dat$AnzBestWir)),
-         caption = "Source: Stadt Zürich Open Data") + 
-    ylim(c(0, max(aggregate(AnzBestWir ~ Year + SexCd + AlterV20Sort, 
-                            data = subset(data, QuarLang == qname & StichtagDatMM == month), sum)$AnzBestWir))) +
-    theme(panel.spacing = unit(0, "lines"),  panel.border = element_blank(), 
-          panel.background = element_rect(fill = "transparent", colour = "transparent"),
-          plot.background = element_rect(fill = "transparent", colour = NA),
-          panel.grid.major.y = element_line(colour = "#e8e8e8"), 
-          panel.grid.major.x = element_blank(), 
-          panel.grid.minor.y = element_blank(), 
-          panel.grid.minor.x = element_blank(),
-          axis.line.y = element_blank(), 
-          axis.line.x = element_blank(), 
-          axis.ticks.x = element_blank(), 
-          axis.ticks.y = element_line(colour = "#aaaaaa"),
-          axis.text.x = element_blank(),
-          legend.background = element_rect(fill = "transparent", colour = "transparent"))
+calc_score <- function(dat, profile) {
+  # create spatial points data frame
+  matrix <- as.matrix(dat)
+  matrix[is.na(matrix)] <- -100
+  score <- matrix %*% profile
+  return(score)
 }
-
--- shiny/server.R
+++ shiny/server.R
@@ -1,7 +1,6 @@
 library(shinydashboard)
 library(shiny)
 library(leaflet)
-library(rgdal)
 library(ggplot2)
 library(maptools)
 library(tmap)
@@ -9,24 +8,50 @@ library(tmaptools)
 library(raster)
 library(osmdata)
 library(sf)
-# prepare data ------------------------------------------------------------
-
-# options(encoding = "UTF-8")
-# map0 <- spTransform(readOGR("./data/GEN_A4_GEMEINDEN_SEEN_2018_F.shp", encoding = "UTF-8", verbose=TRUE),
-#                     CRS("+proj=longlat"))
-# pop <- read.csv("population_raster_ha.csv")
-# emp <- read.csv("empl_raster_ha.csv", sep = ";")
-# save.image("data.RData")
 # server ------------------------------------------------------------------
-load("data.RData")
+load("./data/Criteria_per_Cell.RData")
 source("helpers.R")
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-  output$visplot <- renderLeaflet({
-    tm <- tm_shape(pop_layers) +
-      tm_raster(input$var_pop, palette = "Spectral", title = "Inhabitants per 100m2", n = 10) + qtm(st_geometry(osmdata_sf(add_osm_feature(opq = opq(bbox = c(8.35768, 47.15944,8.984941,47.694472)), key = 'amenity', value = input$var_os))$osm_points))
-    tmap_leaflet(tm) 
+  
+  output$out_var_os <- renderUI({
+    if (input$add_varos){
+      selectInput("var_os", label = "Points of Interest",
+                  choices = c("bar","kindergarten"), selected = "bars")} else {}
+  })
+  
+    output$visplot <- renderLeaflet({
+    user <- c(table(base::cut(input$var_age, breaks = c(-Inf, 6, 15, 19, 24, 44, 64, 79, Inf))), # age
+              if(input$var_gen == "Male"){c(1,0)}else if(input$var_gen == "Female"){c(0,1)}else{c(0,0)}, # gender
+              if(input$var_nat == "Swiss"){c(1,0)}else if(input$var_nat == "Foreign"){c(0,1)}else{c(0,0)}, # nat
+              sapply(c("ht_per","widl_per","handel_per","finanz_per","freiedl_per",
+                       "gewerbe_per","gesundheit_per","bau_per","sonstdl_per","inform_per",    
+                       "unterricht_per","verkehr_per","uebrige_per"), function(x) x == input$var_work)) # work
+    
+    dat$score <- calc_score(dat[,3:27], as.vector(user))
+    coordinates(dat) <- ~ ha_x + ha_y
+    # coerce to SpatialPixelsDataFrame
+    gridded(dat) <- TRUE
+    
+    # plot(pop)
+    # Generate RasterBrick object containing all the layers
+    pop_layers <- brick(dat)
+    
+    #define crs & projection, set to swiss projection
+    crs(pop_layers) <- "+init=epsg:2056" 
+    
+    #transform to WGS84
+    pop_layers2 <- projectRaster(pop_layers, crs="+init=epsg:4326 +proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +units=m +no_defs")
+    
+    tm <- tm_shape(pop_layers2) +
+      tm_raster("score", palette = "Spectral", title = "Inhabitants per 100m2", n = 10) 
+    if (input$add_varos){
+      req(input$var_os)
+      tm <- tm + qtm(st_geometry(osmdata_sf(add_osm_feature(opq = opq(bbox = c(8.35768, 47.15944,8.984941,47.694472)), key = 'amenity', value = input$var_os))$osm_points),
+                     symbols.size = 0.05)
+    } 
+      tmap_leaflet(tm) 
       
   })
   
